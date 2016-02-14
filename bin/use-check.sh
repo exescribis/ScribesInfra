768c45902ab47f97fd9a2c2d31221f8d852a44e9
escribis <escribis@users.noreply.github.com>
2016-02-14 13:36:28 +0100

    misc

--- bin/use-check.sh
+++ bin/use-check.sh
@@ -1,8 +1,9 @@
 # Check a scenario or a state against the model.
 # Should be executed in the directory containing the .use file
+# This script is kept intentionally simple
 # Examples:
-#     use-check scenario CyberGarage Scn001Alicia
-#     use-check state CyberGarage State003Feb12 -q
+#     use-check.sh scenario CyberGarage Scn001Alicia
+#     use-check.sh state CyberGarage State003Feb12 -q
 #
 
 SOILKIND=$1
@@ -10,5 +11,5 @@ CASE_STUDY=$2
 SOIL=$3
 OPTIONS=$4
 
-echo quit | use -nogui -nr ${CASE_STUDY}.use $OPTION ${SOILKIND?}/${CASE_STUDY}${SOIL}.soil
+echo quit | use -nogui -nr ${CASE_STUDY}.use $OPTION ${SOILKIND?}s/${CASE_STUDY}${SOIL}.soil 2>&1
 
