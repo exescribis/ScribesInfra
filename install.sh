eeec99f7e7557f4d8495a3c2ed421d3ecba7cdc7
escribis <escribis@users.noreply.github.com>
2018-01-18 17:15:41 +0100

    update ScribesZone/SphinxZone

---
+++ install.sh
@@ -0,0 +1,13 @@
+VENV=$PWD/venv/
+mkdir -p $VENV
+for reqfile in `ls requirements-*.txt`
+do
+    echo "======= Installing   $reqfile  ======================================"
+    echo pip install --upgrade --target=$VENV --install-option="--install-scripts=$VENV/bin" -r ${reqfile?}
+done
+
+for file in `ls bin`
+do
+    echo +x bin/$file
+    chmod +x bin/$file
+done
